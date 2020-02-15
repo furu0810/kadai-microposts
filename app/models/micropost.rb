9f5ba93675c26546f5f4fae33671d7bb396d1d8a
furu <githubfu093@gmail.com>
2020-02-15 09:55:19 +0000

    ルーディングとfavoritesコントローラーの編集

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -7,20 +7,7 @@ class Micropost < ApplicationRecord
   has_many :favorites
   has_many :users, through: :favorites, source: :user
   
-  def likes(other_micropost)
-    unless self == other_micropost
-     self.favorites.find_or_create_by(micropost_id: other_micropost.id)
-    end
-  end
-  
-  def unlike(other_micropost)
-    favorite = self.favorites.find_by(micropost_id: other_micropost.id)
-    favorite.destroy if favorite
-  end
-  
-  def likes?(other_micropost)
-    self.favorites.include(other_micropost)
-  end
+ 
       
 end
 
